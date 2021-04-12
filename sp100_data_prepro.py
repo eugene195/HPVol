d278a5d2aa5b29ceb0fbb03889ece93d3514ab09
eugene <eugene.07@list.ru>
2021-04-12 15:15:33 +0100

    adds otm analysis

--- sp100_data_prepro.py
+++ sp100_data_prepro.py
@@ -35,18 +35,30 @@ spx_dec_exp["Mid_IV"] = py_vollib_vectorized.vectorized_implied_volatility(
     spx_dec_exp["Call_Put"].str.lower(), q=0, model='black_scholes_merton', return_as='numpy', on_error='ignore'
 )
 
+spx_dec_exp["Delta"] = py_vollib_vectorized.greeks.delta(
+    spx_dec_exp["Call_Put"].str.lower(), spx_dec_exp["Underlying_mid_price"], spx_dec_exp["Price_strike"],
+    spx_dec_exp["Time_to_expiry"], 0.0, spx_dec_exp["Mid_IV"], q=0,
+    model='black_scholes_merton', return_as='numpy'
+)
+
+put_lb, put_rb = -0.25 * 1.1, -0.25 * 0.9
+call_lb, call_rb = 0.25 * 0.9, 0.25 * 1.1
+call_50d_lb, call_50d_rb = 0.5 * 0.9, 0.5 * 1.1
+ticks_25d_put = spx_dec_exp.loc[(spx_dec_exp["Call_Put"] == "P") & ((spx_dec_exp['Delta'] > put_lb) & (spx_dec_exp['Delta'] < put_rb))]
+ticks_25d_call = spx_dec_exp.loc[(spx_dec_exp["Call_Put"] == "C") & ((spx_dec_exp['Delta'] > call_lb) & (spx_dec_exp['Delta'] < call_rb))]
+ticks_50d_call = spx_dec_exp.loc[(spx_dec_exp["Call_Put"] == "C") & ((spx_dec_exp['Delta'] > call_50d_lb) & (spx_dec_exp['Delta'] < call_50d_rb))]
+
 # atm range: https://repositorio.ucp.pt/bitstream/10400.14/29052/1/Dissertation%20Hanna%20Nikanorova.pdf
 # 0.975 and 1.025
-ticks_atm_log_mon = spx_dec_exp.loc[(spx_dec_exp['Price_strike'] < spx_dec_exp['Underlying_mid_price'] * 1.025) &
-                                    (spx_dec_exp['Price_strike'] > spx_dec_exp['Underlying_mid_price'] * 0.975)]
+# ticks_atm_log_mon = spx_dec_exp.loc[(spx_dec_exp['Price_strike'] < spx_dec_exp['Underlying_mid_price'] * 1.025) &
+#                                     (spx_dec_exp['Price_strike'] > spx_dec_exp['Underlying_mid_price'] * 0.975)]
+#
+# print(len(ticks_atm_log_mon))
+# print("Put vs Call count")
+# print(len(ticks_atm_log_mon.loc[(ticks_atm_log_mon['Call_Put'] == "P")]))
+# print(len(ticks_atm_log_mon.loc[(ticks_atm_log_mon['Call_Put'] == "C")]))
 
-print(len(ticks_atm_log_mon))
-print("Put vs Call count")
-print(len(ticks_atm_log_mon.loc[(ticks_atm_log_mon['Call_Put'] == "P")]))
-print(len(ticks_atm_log_mon.loc[(ticks_atm_log_mon['Call_Put'] == "C")]))
-
-ticks_atm_log_mon.reset_index(drop=True, inplace=True)
-
-ticks_atm_log_mon = ticks_atm_log_mon[raw_ds_columns()]
-
-ticks_atm_log_mon.to_csv("data/current.csv", )
+for dataset_name, dataset in zip(["ticks_25dp", "ticks_25dc", "ticks_50dc"], [ticks_25d_put, ticks_25d_call, ticks_50d_call]):
+    dataset.reset_index(drop=True, inplace=True)
+    ds_to_csv = dataset[raw_ds_columns()]
+    ds_to_csv.to_csv("data/{}_sp100.csv".format(dataset_name), )
